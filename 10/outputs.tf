f5db41884e45e060d4d4f4b2a77f08c67d15b532
Vikash Kumar <kumarvikash97716880@gmail.com>
2026-08-10 19:48:53 +0530

    file add

---
+++ 10/outputs.tf
@@ -0,0 +1,67 @@
+
+# ==============================================================================
+# EXAMPLE 2: DYNAMIC BLOCKS
+# ==============================================================================
+# Uncomment the block below to test dynamic blocks
+# This example creates multiple security group rules from a list variable
+# - No need to repeat ingress blocks manually
+# - Add/remove rules by editing the ingress_rules variable
+# ==============================================================================
+
+# resource "aws_security_group" "dynamic_sg" {
+#   name        = "dynamic-sg-${var.environment}"
+#   description = "Security group with dynamic rules"
+  
+#   # Dynamic block creates multiple ingress rules from a list
+#   dynamic "ingress" {
+#     for_each = var.ingress_rules
+#     content {
+#       from_port   = ingress.value.from_port
+#       to_port     = ingress.value.to_port
+#       protocol    = ingress.value.protocol
+#       cidr_blocks = ingress.value.cidr_blocks
+#       description = ingress.value.description
+#     }
+#   }
+  
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+  
+#   tags = {
+#     Name = "dynamic-sg-${var.environment}"
+#   }
+# }
+
+# ==============================================================================
+# EXAMPLE 3: SPLAT EXPRESSIONS
+# ==============================================================================
+# Uncomment the blocks below to test splat expressions
+# This example creates multiple instances and uses splat [*] to extract values
+# - Creates 'instance_count' number of instances
+# - Extracts all IDs and IPs in a single expression
+# ==============================================================================
+
+# resource "aws_instance" "splat_example" {
+#   count = var.instance_count
+  
+#   ami           = data.aws_ami.amazon_linux.id
+#   instance_type = "t2.micro"
+  
+#   tags = {
+#     Name = "instance-${count.index + 1}"
+#   }
+# }
+
+# # Use splat expressions to extract values from all instances
+# locals {
+#   # Get all instance IDs in one line using [*]
+#   all_instance_ids = aws_instance.splat_example[*].id
+  
+#   # Get all private IPs using [*]
+#   all_private_ips = aws_instance.splat_example[*].private_ip
+# }
+
